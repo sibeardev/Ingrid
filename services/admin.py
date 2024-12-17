e5072ba0c792f924524ca9ef5d270d43ec9791a6
18372738 <133884450+18372738@users.noreply.github.com>
2024-12-18 00:37:18 +0300

    added models for the application services

--- services/admin.py
+++ services/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Client
+from .models import Salon
+from .models import Service
+from .models import Specialist
+from .models import Order
+from .models import SpecialistWorkDayInSalon
+from .models import Appointment
+
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Salon)
+class SalonAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Specialist)
+class SpecialistAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(SpecialistWorkDayInSalon)
+class SpecialistWorkDayInSalonAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Appointment)
+class AppointmentAdmin(admin.ModelAdmin):
+    pass
--- services/models.py
+++ services/models.py
@@ -1,1 +1,186 @@
 
+from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+
+
+class Client(models.Model):
+    """Клиент"""
+
+    phone_number = PhoneNumberField("Номер телефона", null=True, region="RU")
+    full_name = models.CharField("ФИО", max_length=200)
+
+    class Meta:
+        verbose_name = 'Клиент'
+        verbose_name_plural = 'Клиенты'
+
+    def __str__(self) -> str:
+        return self.full_name
+
+
+class Salon(models.Model):
+    """Салон"""
+
+    title = models.CharField("Название салона", max_length=100)
+    address = models.TextField("Адрес салона", max_length=200)
+
+    class Meta:
+        verbose_name = 'Салон'
+        verbose_name_plural = 'Салоны'
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class Service(models.Model):
+    """Услуга"""
+
+    title = models.CharField("Название услуги", max_length=200)
+    price = models.FloatField("Цена услуги")
+    duration = models.IntegerField("Длительность услуги, мин")
+
+    class Meta:
+        verbose_name = 'Услуга'
+        verbose_name_plural = 'Услуги'
+
+    def __str__(self) -> str:
+        return self.title
+
+
+class Specialist(models.Model):
+    """Специалист"""
+
+    full_name = models.CharField("ФИО", max_length=200)
+    type_service = models.CharField("Вид услуги", max_length=50)
+    work_experience_years = models.IntegerField("Стаж (годы)", default=0)
+    work_experience_months = models.IntegerField("Стаж (месяцы)", default=0)
+
+    class Meta:
+        verbose_name = 'Специалист'
+        verbose_name_plural = 'Специалисты'
+
+    def __str__(self) -> str:
+        return self.full_name
+
+
+class Order(models.Model):
+    """Счёт (статус оплаты)"""
+
+    STATUS = [
+        ("waiting", "Ожидает оплаты"),
+        ("paid", "Оплачено"),
+        ("cancel", "Отменено"),
+    ]
+
+    client = models.ForeignKey(
+        Client,
+        on_delete=models.PROTECT,
+        verbose_name="Клиент",
+        related_name="orders"
+    )
+    status = models.CharField(
+        "Статус заказа",
+        max_length=14,
+        choices=STATUS,
+        default="waiting"
+    )
+    total_amount = models.DecimalField(
+        "Сумма заказа",
+        max_digits=10,
+        decimal_places=2,
+        blank=True,
+        null=True
+    )
+    receipt = models.URLField("Чек", blank=True)
+    created_at = models.DateTimeField("Счёт выставлен", auto_now_add=True)
+    updated_at = models.DateTimeField("Последнее обновление", auto_now=True)
+
+    class Meta:
+        verbose_name = 'Счет оплаты'
+        verbose_name_plural = 'Счета оплаты'
+
+    def __str__(self) -> str:
+        return f"{self.updated_at} {self.client.full_name} {self.status}"
+
+
+class SpecialistWorkDayInSalon(models.Model):
+    """Рабочий день сотрудника в одном из салонов"""
+
+    workday = models.DateField("Дата рабочего дня")
+    salon = models.ForeignKey(
+        Salon,
+        on_delete=models.PROTECT,
+        verbose_name="Салон",
+        related_name="workdays"
+    )
+    specialist = models.ForeignKey(
+        Specialist,
+        on_delete=models.PROTECT,
+        verbose_name="Специалист",
+        related_name="workdays"
+    )
+    services = models.ManyToManyField(Service, verbose_name="Услуги",)
+    start_at = models.TimeField("Начало рабочего дня")
+    end_at = models.TimeField("Окончание рабочего дня")
+
+    class Meta:
+        verbose_name = 'Рабочий день'
+        verbose_name_plural = 'Рабочие дни'
+
+    def __str__(self) -> str:
+        return f"{self.specialist} {self.salon} {self.workday}"
+
+
+class Appointment(models.Model):
+    """Заказ услуги"""
+
+    STATUSES = [
+        ("accepted", "Принято"),
+        ("ended", "Завершено"),
+        ("discard", "Отменено"),
+    ]
+
+    status = models.CharField(
+        "Статус заказа",
+        max_length=9,
+        choices=STATUSES,
+        default="accepted"
+    )
+    salon = models.ForeignKey(
+        Salon,
+        on_delete=models.PROTECT,
+        verbose_name="Салон",
+        related_name="appointments"
+    )
+    client = models.ForeignKey(
+        Client,
+        on_delete=models.PROTECT,
+        verbose_name="Клиент",
+        related_name="appointments"
+    )
+    specialist = models.ForeignKey(
+        Specialist,
+        on_delete=models.PROTECT,
+        verbose_name="Специалист",
+        related_name="appointments"
+    )
+    service = models.ForeignKey(
+        Service,
+        on_delete=models.PROTECT,
+        verbose_name="Услуга",
+        related_name="appointments"
+    )
+    order = models.ForeignKey(
+        Order,
+        on_delete=models.PROTECT,
+        verbose_name="Заказ",
+        related_name="appointments"
+    )
+    date = models.DateField("Дата записи")
+    start_at = models.TimeField("Время начала")
+
+    class Meta:
+        verbose_name = 'Заказ услуги'
+        verbose_name_plural = 'Заказ услуг'
+
+    def __str__(self) -> str:
+        return f"{self.status} {self.date} {self.salon.title} {self.client.full_name}"
